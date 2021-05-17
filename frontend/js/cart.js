12cefd91f53b88d5eb3f2350741037377c649467
Camille Chouteau <camille.chouteau@gmail.com>
2021-05-17 12:09:09 +0200

    Populate cart page with products in cart

--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -1,13 +1,29 @@
 import { updateCartProductsNumber } from "./helpers/helpers.js";
 import { getCart, saveCart } from "./helpers/cart.js";
+import { findProduct } from "./helpers/product.js";
 
 const cart = getCart();
-for (const productId of cart) {
-    console.log(productId);
-}
+for (const savedProduct of cart) {
+    findProduct(savedProduct.id).then((product) => {
+        const originalProduct = document.getElementById("product-template");
+        const clonedProduct = document.importNode(
+            originalProduct.content,
+            true
+        );
 
-const getAndPopulateProduct = () => {
-    fetch("http://localhost:3000/api/furniture");
-};
+        const productImage = clonedProduct.getElementById("productImage");
+        const productName = clonedProduct.getElementById("productName");
+        const productVarnish = clonedProduct.getElementById("productVarnish");
+        const productPrice = clonedProduct.getElementById("productPrice");
+
+        productImage.setAttribute("src", product.imageUrl);
+        productName.textContent = product.name;
+        productVarnish.textContent = savedProduct.varnish;
+        productPrice.textContent = product.price / 100 + "€";
+
+        const tableProducts = document.getElementById("tableProducts");
+        tableProducts.appendChild(clonedProduct);
+    });
+}
 
 updateCartProductsNumber();
--- frontend/js/product.js
+++ frontend/js/product.js
@@ -68,8 +68,12 @@ const displayProduct = (product) => {
         alertVarnish.style.display = "none";
         // On appelle la fonction getCart qui est dans le fichier cart.js dans le dossier helpers
         const cart = getCart();
+        const savedProduct = {
+            id: product._id,
+            varnish: productVarnish.value,
+        };
         // On ajoute au tableau (stocké dans la variable cart et qui représente la panier) l'id du produit que le client souhaite ajouter à son panier
-        cart.push(product._id);
+        cart.push(savedProduct);
         // On appelle la fonction saveCart qui est dans le fichier cart.js dans le dossier helpers
         // On donne en argument cart pour pouvoir enregistrer le panier
         saveCart(cart);
