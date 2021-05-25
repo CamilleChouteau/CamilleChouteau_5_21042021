418bb8f4593f49d753f14fe51b5df06e5346bb03
Camille Chouteau <camille.chouteau@gmail.com>
2021-05-25 21:38:01 +0200

    Finished order confirmation page

--- frontend/js/order-confirmation.js
+++ frontend/js/order-confirmation.js
@@ -1,3 +1,11 @@
 import { updateCartProductsNumber } from "./helpers/helpers.js";
 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const orderId = urlParams.get("orderId");
+const customerName = urlParams.get("firstName");
+
+document.getElementById("customerName").textContent = customerName;
+document.getElementById("orderNumber").textContent = orderId;
+
 updateCartProductsNumber();
